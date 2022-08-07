7cadbd705d43ed49c4ff83ecfe1315e56998146d
Emeline Van der Beken <e.vanderbeken@mediascale.eu>
2022-08-07 09:50:41 +0200

    Add: streamlit authenticator form to access to the app
    - Defined method in main page module to connect to the database and to authenticate to the app.
    - Added conditions in subpages modules to access to the app.

--- 01_🏠 Accueil.py
+++ 01_🏠 Accueil.py
@@ -1,21 +1,67 @@
 import streamlit as st
-from streamlit_page import Page
 from streamlit import cli as stcli
 import sys
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
+
+from user_db import UserDatabase
+from streamlit_authenticator import StreamlitAuthenticator
+from streamlit_page import Page
 
 st.set_page_config(layout="wide", page_title="Home", page_icon='🏠',
                            initial_sidebar_state="expanded")
 
 
 class Home(Page):
+    def __init__(self):
+        super(Home, self).__init__()
+        self.db = self.connect_to_db()
+        self.user_db = UserDatabase(self.db)
+        USERS_TABLE = self.user_db.return_table_as_df()
+        self.authenticator = StreamlitAuthenticator(USERS_TABLE)
+
+    @staticmethod
+    @st.cache(allow_output_mutation=True)
+    def connect_to_db():
+        """
+        Connect to postgres database containing users credentials.
+
+        Returns
+        -------
+
+        """
+        load_dotenv()
+
+        db = sqlalchemy.create_engine(
+            sqlalchemy.engine.url.URL.create(
+                drivername='postgresql',
+                username=os.getenv('USERNAME'),
+                password=os.getenv('PASSWORD'),
+                host=os.getenv('HOST'),
+                port=os.getenv('PORT'),
+                database=os.getenv('DATABASE'),
+            ),
+            echo_pool=True,
+        )
+
+        base = declarative_base()
+        base.metadata.create_all(db)
+        return db
+
+    def authentication(self):
+        self.authenticator.authentication(main_app_fct_to_show=self.show_home)
+
     @staticmethod
     def show_home():
         st.markdown("# Accueil")
+        st.write(f"Bienvenue {st.session_state['name']}")
 
 
 def main():
     app = Home()
-    app.show_home()
+    app.authentication()
 
 
 if __name__ == "__main__":
